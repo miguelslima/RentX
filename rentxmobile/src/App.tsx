57d3cd7b9b5c396b4da235b53b688bff4b6c4ec6
Miguel <miguelsousalima@hotmail.com>
2020-12-15 10:47:55 -0300

    Onboarding Ok

--- rentxmobile/src/App.tsx
+++ rentxmobile/src/App.tsx
@@ -1,10 +1,63 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View, StatusBar, AsyncStorage, Image } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
 
 // import { Container } from './styles';
 
+// const completeOnboarding = async () => {
+//   await AsyncStorage.setItem(
+//     "rentX",
+//     JSON.stringify({
+//       rentX: true,
+//     })
+//   );
+//   navigation.navigate("Login");
+// };
+
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <StatusBar
+        translucent={true}
+        backgroundColor="#1b1b1b"
+        barStyle="light-content"
+      />
+      <Onboarding
+        pages={[
+          {
+            backgroundColor: "#1b1b1b",
+            image: <Image source={require("./assets/Splash/iPhone.png")} />,
+            title: "",
+            subtitle: "",
+          },
+          {
+            backgroundColor: "#1b1b1b",
+            image: <Image source={require("./assets/Splash/Union.png")} />,
+            title: "",
+            subtitle: "",
+          },
+          {
+            backgroundColor: "#1b1b1b",
+            image: <Image source={require("./assets/Splash/iPhone.png")} />,
+            title: "",
+            subtitle: "",
+          },
+          {
+            backgroundColor: "#1b1b1b",
+            image: <Image source={require("./assets/Splash/Logotipo.png")} />,
+            title: "",
+            subtitle: "",
+          },
+          {
+            backgroundColor: "#1b1b1b",
+            image: <Image source={require("./assets/Splash/iPhone.png")} />,
+            title: "",
+            subtitle: "",
+          },
+        ]}
+      />
+    </>
+  );
 };
 
 export default App;
