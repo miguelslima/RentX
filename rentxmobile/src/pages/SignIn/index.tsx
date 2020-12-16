fdde19b4e7fe72904f836520b77536cb9cff2ec0
Miguel <miguelsousalima@hotmail.com>
2020-12-16 00:02:24 -0300

    Login started

--- rentxmobile/src/pages/SignIn/index.tsx
+++ rentxmobile/src/pages/SignIn/index.tsx
@@ -1,25 +1,65 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useCallback, useState, useRef } from "react";
+import { Form } from "@unform/mobile";
+import { FormHandles } from "@unform/core";
 import Icon from "react-native-vector-icons/Feather";
-import { Container, KeyboardAvoidingContainer, GoBack, HeaderContainer, Title, Subtitle } from "./styles";
+import Button from "../../components/Button";
+import {
+  Container,
+  KeyboardAvoidingContainer,
+  GoBack,
+  HeaderContainer,
+  Title,
+  Subtitle,
+  InputContainer,
+  TextContainer,
+} from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { Alert, Text } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+interface SubmitForm {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmitForm = () => {
+    Alert.alert("Teste");
+  };
+
   return (
     <Container>
       <KeyboardAvoidingContainer behavior="position">
-        <GoBack>
+        <GoBack onPress={() => navigation.goBack()}>
           <Icon size={20} name="chevron-left" color="#AEAEB3" />
         </GoBack>
         <HeaderContainer>
-          <Title>
-            Estamos quase lá
-          </Title>
+          <Title>Estamos quase lá</Title>
           <Subtitle>
             Faça seu login para começar uma experiência incrível
           </Subtitle>
         </HeaderContainer>
 
+        <Form ref={formRef} onSubmit={handleSubmitForm}>
+          <InputContainer>
+            <TextInput placeholder="E-mail" />
+            <TextInput placeholder="Senha" />
+          </InputContainer>
+          <TextContainer>
+            <Text>Lembrar-me</Text>
+            <Text> Esqueci minha senha</Text>
+          </TextContainer>
+        </Form>
 
+        <Button
+          style={{ width: "100%" }}
+          text="Login"
+          enable
+          onPress={() => {}}
+        />
       </KeyboardAvoidingContainer>
     </Container>
   );
--- rentxmobile/src/pages/SignIn/styles.ts
+++ rentxmobile/src/pages/SignIn/styles.ts
@@ -4,16 +4,21 @@ export const Container = styled.View`
   flex: 1;
   background-color: #fff;
   justify-content: space-between;
-  padding-horizontal: 32px;
+
 `;
 
 export const KeyboardAvoidingContainer = styled.KeyboardAvoidingView`
   flex: 1;
+
 `;
 
-export const GoBack = styled.TouchableOpacity``;
+export const GoBack = styled.TouchableOpacity`
+padding-horizontal: 32px;
+`;
 
-export const HeaderContainer = styled.View``;
+export const HeaderContainer = styled.View`
+padding-horizontal: 32px;
+`;
 
 export const Title = styled.Text`
   width: 169px;
@@ -29,4 +34,16 @@ export const Subtitle = styled.Text`
   line-height: 25px;
   font-family: "Inter-Regular";
   margin-top: 24px;
+`;
+
+export const InputContainer = styled.View`
+  margin-top: 110px;
+  padding-horizontal: 32px;
+`;
+
+export const TextContainer = styled.View`
+  margin: 34px 0;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-horizontal: 32px;
 `;
