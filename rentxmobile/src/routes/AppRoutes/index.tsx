780d771fe088837d485b33774a0adabdd7377f7d
Miguel <miguelsousalima@hotmail.com>
2020-12-18 19:32:46 -0300

    Setting Routes Ok

--- rentxmobile/src/routes/AppRoutes/index.tsx
+++ rentxmobile/src/routes/AppRoutes/index.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeTabs from "./HomeTabs";
-import Home from "../../pages/Home";
+import Profile from "../../pages/Profile";
+import Success from "../../pages/SignUp/Success";
 
 const App = createStackNavigator();
 
@@ -14,7 +15,6 @@ const AppRoutes: React.FC = () => (
       cardStyle: { backgroundColor: "#F9F9F9" },
     }}
   >
-    {/* <App.Screen name="Home" component={Home} /> */}
     <App.Screen name="HomeTabs" component={HomeTabs} />
   </App.Navigator>
 );
--- rentxmobile/src/routes/index.tsx
+++ rentxmobile/src/routes/index.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { View, ActivityIndicator } from "react-native";
+
+import SplashScreen from "react-native-splash-screen";
+import AuthRoutes from "./AuthRoutes/";
+import AppRoutes from "./AppRoutes/";
 
 import { useAuth } from "../hooks/authMock";
-
-import AuthRoutes from "./AuthRoutes";
-import AppRoutes from "./AppRoutes";
 
 const Routes: React.FC = () => {
   const { user, loading } = useAuth();
 
-  if (loading === false) {
-    return user ? <AppRoutes /> : <AuthRoutes />;
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#DC1637" />
+      </View>
+    );
   }
-  return null;
+
+  SplashScreen.hide();
+
+  return user ? <AppRoutes /> : <AuthRoutes />;
 };
 
 export default Routes;
