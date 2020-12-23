76ab4e9f4e30652c961f4cb41a7b50788537d3de
Miguel <miguelsousalima@hotmail.com>
2020-12-23 15:41:56 -0300

    Start pages profile

--- rentxmobile/src/pages/Profile/index.tsx
+++ rentxmobile/src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import Button from "../../components/Button";
 
 import Icon from "react-native-vector-icons/AntDesign";
@@ -11,8 +11,16 @@ import {
   HeaderContainer,
   ImageContainer,
   Title,
+  ButtonEdit,
   ButtonSignOut,
   Name,
+  SchedulingContainer,
+  SchedulingText,
+  SchedulingNumber,
+  Line,
+  FavoriteCarContainer,
+  FavoriteCarText,
+  FavoriteCarNumber,
 } from "./styles";
 
 const Profile: React.FC = () => {
@@ -20,22 +28,42 @@ const Profile: React.FC = () => {
   return (
     <Container>
       <HeaderContainer>
-        <Icon name="edit" size={18} color="#AEAEB3" />
+        <ButtonEdit
+          onPress={() => Alert.alert("Edit", "Estamos em manutenção!")}
+        >
+          <Icon name="edit" size={18} color="#AEAEB3" />
+        </ButtonEdit>
         <Title>Perfil</Title>
         <ButtonSignOut onPress={() => signOut()}>
           <Icon name="poweroff" size={20} color="#AEAEB3" />
         </ButtonSignOut>
       </HeaderContainer>
 
-      <ImageContainer
-
-      >
+      <ImageContainer>
         <Icon name="user" size={90} />
       </ImageContainer>
 
-      <View style={{ marginTop: 150 }}>
+      <View style={{ marginTop: 125 }}>
         <Name>Miguel Lima</Name>
       </View>
+
+      <SchedulingContainer>
+        <SchedulingText>Agendamentos feitos</SchedulingText>
+        <SchedulingNumber>05</SchedulingNumber>
+      </SchedulingContainer>
+
+      <Line />
+
+      <FavoriteCarContainer
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginHorizontal: 24,
+        }}
+      >
+        <FavoriteCarText>Carro favorito</FavoriteCarText>
+        <FavoriteCarNumber>Utilizado 2 vezes</FavoriteCarNumber>
+      </FavoriteCarContainer>
     </Container>
   );
 };
--- rentxmobile/src/pages/Profile/styles.ts
+++ rentxmobile/src/pages/Profile/styles.ts
@@ -6,7 +6,7 @@ export const HeaderContainer = styled.View`
   flex-direction: row;
   justify-content: space-between;
 
-  height: 150px;
+  height: 180px;
   padding-horizontal: 27px;
   padding-top: 32px;
   background-color: #1b1b1f;
@@ -16,7 +16,7 @@ export const ImageContainer = styled.View`
   width: 180px;
   height: 180px;
   position: absolute;
-  top: 95px;
+  top: 90px;
   align-self: center;
   justify-content: center;
   align-items: center;
@@ -32,6 +32,53 @@ export const Title = styled.Text`
   color: #fff;
 `;
 
+export const ButtonEdit = styled.TouchableOpacity``;
 export const ButtonSignOut = styled.TouchableOpacity``;
 
-export const Name = styled.Text``;
+export const Name = styled.Text`
+  font-family: "Archivo-Bold";
+  font-size: 30px;
+  line-height: 33px;
+  color: #3d3d4d;
+  text-align: center;
+`;
+
+export const SchedulingContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-horizontal: 24px;
+  margin-top: 64px;
+`;
+
+export const SchedulingText = styled.Text`
+  font-family: "Inter-Regular";
+  font-size: 15px;
+  line-height: 18px;
+  color: #7a7a80;
+`;
+
+export const SchedulingNumber = styled.Text`
+  font-family: "Archivo-Medium";
+  font-size: 15px;
+  line-height: 18px;
+  color: #47474d;
+`;
+
+export const Line = styled.View`
+  margin-top: 19px;
+  margin-bottom: 27px;
+  margin-horizontal: 24px;
+  border-bottom-width: 2px;
+  border-bottom-color: #e6e6f0;
+`;
+
+export const FavoriteCarContainer = styled.View``;
+
+export const FavoriteCarText = styled.Text`
+  color: #7a7a80;
+  font-size: 15px;
+  line-height: 18px;
+  font-family: "Inter-Regular"
+`;
+
+export const FavoriteCarNumber = styled.Text``;
